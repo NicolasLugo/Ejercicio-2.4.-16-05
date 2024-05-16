5fe237c93bc8144a5becb09a6cc99bf3bbc2de09
NicolasLugo <123844622+NicolasLugo@users.noreply.github.com>
2024-05-16 12:26:06 -0400

    BD 16-05 ejercicio.sql

--- BD 16-05 ejercicio.sql
+++ BD 16-05 ejercicio.sql
@@ -14,7 +14,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 -- Volcando estructura de base de datos para 1605NL
 CREATE DATABASE IF NOT EXISTS `1605NL` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `1605NL`;
@@ -32,30 +31,6 @@ CREATE TABLE IF NOT EXISTS `Clientes` (
 INSERT INTO `Clientes` (`id_cliente`, `nombre_cliente`, `direccion_cliente`, `contacto_cliente`) VALUES
 	(1, 'Miguel Zapata', 'Calle falsa 123', '3579512364'),
 	(2, 'Matias Zepeda', 'calle verdadera 456', '325697412');
-
--- Volcando estructura para tabla 1605NL.Cursos
-CREATE TABLE IF NOT EXISTS `Cursos` (
-  `id_curso` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre_curso` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_curso`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla 1605NL.Cursos: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla 1605NL.Cursos_estudiantes
-CREATE TABLE IF NOT EXISTS `Cursos_estudiantes` (
-  `id_curso` int(11) DEFAULT NULL,
-  `id_estudiante` int(11) DEFAULT NULL,
-  `id_profesor` int(11) DEFAULT NULL,
-  KEY `id_curso` (`id_curso`),
-  KEY `id_estudiante` (`id_estudiante`),
-  KEY `id_profesor` (`id_profesor`),
-  CONSTRAINT `Cursos_estudiantes_ibfk_1` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
-  CONSTRAINT `Cursos_estudiantes_ibfk_2` FOREIGN KEY (`id_estudiante`) REFERENCES `Estudiantes` (`id_estudiante`),
-  CONSTRAINT `Cursos_estudiantes_ibfk_3` FOREIGN KEY (`id_profesor`) REFERENCES `Profesores` (`id_profesor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla 1605NL.Cursos_estudiantes: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla 1605NL.Detalle_Pedidos
 CREATE TABLE IF NOT EXISTS `Detalle_Pedidos` (
@@ -89,17 +64,6 @@ INSERT INTO `Empleados` (`id_empleado`, `rut_empleado`, `nombre_empleado`, `carg
 	(1, '123456789', 'Nicolás Lugo', 'Cocinero', 987654321, 'abc@gmail.com'),
 	(2, '365987412', 'Mario Zepeda', 'Mesero', 325697412, 'def@gmail.com');
 
--- Volcando estructura para tabla 1605NL.Estudiantes
-CREATE TABLE IF NOT EXISTS `Estudiantes` (
-  `id_estudiante` int(11) NOT NULL AUTO_INCREMENT,
-  `rut_estudiante` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_estudiante` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `nacionalidad` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_estudiante`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla 1605NL.Estudiantes: ~0 rows (aproximadamente)
-
 -- Volcando estructura para tabla 1605NL.Inventario
 CREATE TABLE IF NOT EXISTS `Inventario` (
   `id_articulo_inv` int(11) NOT NULL AUTO_INCREMENT,
@@ -132,16 +96,6 @@ CREATE TABLE IF NOT EXISTS `Pedidos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Volcando datos para la tabla 1605NL.Pedidos: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla 1605NL.Profesores
-CREATE TABLE IF NOT EXISTS `Profesores` (
-  `id_profesor` int(11) NOT NULL AUTO_INCREMENT,
-  `rut_profesor` varchar(13) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre_profesor` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_profesor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Volcando datos para la tabla 1605NL.Profesores: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla 1605NL.Sucursales
 CREATE TABLE IF NOT EXISTS `Sucursales` (
